3ea930d414ce1dc8ec84bdb74a28dc07f843d0dc
Jonathan Kelly <50jkelly@gmail.com>
2016-12-20 18:27:42 +0000

    Started moving window creation to functions so multiple windows can easily be created2

--- src/crossing.asm
+++ src/crossing.asm
@@ -4,130 +4,134 @@ entry start
 include 'win32a.inc'
 
 section '.code' code readable executable
-	start:
-		; Initialise the stack frame
 
-		push ebp
-		mov ebp, esp
-		sub esp, 8
+		InitialiseWindow:
 
-		; Initialise the window
+			; Initialise the stack frame
 
-		mov dword [esp], NULL
-		call [GetModuleHandle]
-		mov dword [wcex.hInstance], eax
-		mov dword [esp], NULL
-		mov dword [esp+4], IDI_APPLICATION
-		call [LoadIcon]
-		test eax, eax
-		jz loadIconError
-		mov dword [wcex.hIcon], eax
-		mov dword [wcex.hIconSm], eax
-		mov dword [esp], NULL
-		mov dword [esp+4], IDC_ARROW
-		call [LoadCursor]
-		mov dword [wcex.hCursor], eax
-		mov dword [wcex.hbrBackground], COLOR_WINDOW+1
-		mov dword [wcex.lpszClassName], szClass
-		mov dword [wcex.lpszMenuName], NULL
-		mov	eax, sizeof.WNDCLASSEX
-		mov dword [wcex.cbSize], eax
-		mov dword [wcex.style], CS_HREDRAW+CS_VREDRAW
-		mov dword [wcex.lpfnWndProc], WndProc
-		mov dword [wcex.cbClsExtra], 0
-		mov dword [wcex.cbWndExtra], 0
-		mov dword [esp], wcex
-		call [RegisterClassEx]
-		test eax, eax
-		jz regError
+			push ebp
+			mov ebp, esp
 
-		; Create the window
+			; Initialise the window
 
-		sub esp, 48
-		mov dword [esp], 0
-		mov dword [esp+4], szClass
-		mov dword [esp+8], applicationTitle
-		mov dword [esp+12], WS_OVERLAPPEDWINDOW
-		mov dword [esp+16], CW_USEDEFAULT
-		mov dword [esp+20], CW_USEDEFAULT
-		mov dword [esp+24], 240
-		mov dword [esp+28], 120
-		mov dword [esp+32], NULL
-		mov dword [esp+36], NULL
-		mov dword [esp+40], wcex.hInstance
-		mov dword [esp+44], NULL
-		call [CreateWindowEx]
-		test eax, eax
-		jz createError
-		mov [windowHandle], eax
-	
-		; Show the window
+			sub esp, 4
+			mov dword [esp], NULL
+			call [GetModuleHandle]
+			mov dword [wcex.hInstance], eax
+			sub esp, 8
+			mov dword [esp], NULL
+			mov dword [esp+4], IDI_APPLICATION
+			call [LoadIcon]
+			test eax, eax
+			jz loadIconError
+			mov dword [wcex.hIcon], eax
+			mov dword [wcex.hIconSm], eax
+			sub esp, 8
+			mov dword [esp], NULL
+			mov dword [esp+4], IDC_ARROW
+			call [LoadCursor]
+			mov dword [wcex.hCursor], eax
+			mov dword [wcex.hbrBackground], COLOR_WINDOW+1
+			mov dword [wcex.lpszClassName], szClass
+			mov dword [wcex.lpszMenuName], NULL
+			mov	eax, sizeof.WNDCLASSEX
+			mov dword [wcex.cbSize], eax
+			mov dword [wcex.style], CS_HREDRAW+CS_VREDRAW
+			mov dword [wcex.lpfnWndProc], WndProc
+			mov dword [wcex.cbClsExtra], 0
+			mov dword [wcex.cbWndExtra], 0
+			sub esp, 4
+			mov dword [esp], wcex
+			call [RegisterClassEx]
+			test eax, eax
+			jz regError
 
-		sub esp, 8
-		mov eax, [windowHandle]
-		mov dword [esp], eax
-		mov dword [esp+4], SW_SHOWNORMAL
-		call [ShowWindow]
+			pop ebp
+			ret
 
-		; Update the window
+		NewWindow:
 
-		sub esp, 4
-		mov eax, [windowHandle]
-		mov dword [esp], eax
-		call [UpdateWindow]
+			push ebp
+			mov ebp, esp
 
-	messageLoop:
-		sub esp, 16
-		mov dword [esp], msg
-		mov dword [esp+4], NULL
-		mov dword [esp+8], 0
-		mov dword [esp+12], 0
-		call [GetMessage]
-		test eax, eax
-		jz exit
-		sub esp, 4
-		mov dword [esp], msg
-		call [TranslateMessage]
-		sub esp, 4
-		mov dword [esp], msg
-		call [DispatchMessage]
-		jmp messageLoop
+			; Create the window
 
-	loadIconError:
-		sub esp, 4
-		mov dword [esp], loadIconErrorMessage
-		call ErrorMessageBox
-		jmp exit
+			sub esp, 48
+			mov dword [esp], 0
+			mov dword [esp+4], szClass
+			mov dword [esp+8], applicationTitle
+			mov dword [esp+12], WS_OVERLAPPEDWINDOW
+			mov dword [esp+16], CW_USEDEFAULT
+			mov dword [esp+20], CW_USEDEFAULT
+			mov dword [esp+24], 240
+			mov dword [esp+28], 120
+			mov dword [esp+32], NULL
+			mov dword [esp+36], NULL
+			mov dword [esp+40], wcex.hInstance
+			mov dword [esp+44], NULL
+			call [CreateWindowEx]
+			test eax, eax
+			jz createError
+			mov [windowHandle], eax
+		
+			; Show the window
 
-	regError:
-		sub esp, 4
-		mov dword [esp], registerClassErrorMessage
-		call ErrorMessageBox
-		jmp exit
+			sub esp, 8
+			mov eax, [windowHandle]
+			mov dword [esp], eax
+			mov dword [esp+4], SW_SHOWNORMAL
+			call [ShowWindow]
 
-    createError:
-		sub esp, 4
-		mov dword [esp], createWindowErrorMessage
-		call ErrorMessageBox
-		jmp exit
+		messageLoop:
+			sub esp, 16
+			mov dword [esp], msg
+			mov eax, [windowHandle]
+			mov dword [esp+4], NULL
+			mov dword [esp+8], 0
+			mov dword [esp+12], 0
+			call [GetMessage]
+			cmp eax, 0
+			je exit
+			sub esp, 4
+			mov dword [esp], msg
+			call [TranslateMessage]
+			sub esp, 4
+			mov dword [esp], msg
+			call [DispatchMessage]
+			jmp messageLoop
 
-    exit:
-		sub esp, 4
-		mov eax, [msg.wParam]
-		mov dword [esp], eax
-		call [ExitProcess]
+		loadIconError:
+			sub esp, 4
+			mov dword [esp], loadIconErrorMessage
+			call ErrorMessageBox
+			jmp exit
+
+		regError:
+			sub esp, 4
+			mov dword [esp], registerClassErrorMessage
+			call ErrorMessageBox
+			jmp exit
+
+		createError:
+			sub esp, 4
+			mov dword [esp], createWindowErrorMessage
+			call ErrorMessageBox
+			jmp exit
+
+		exit:
+			pop ebp
+			mov eax, [msg.wParam]
+			ret
 
     WndProc:
 		push ebp
 		mov ebp, esp
+
+		mov eax, [ebp+12]
+		cmp eax, WM_DESTROY
+		je destroy
+
 		sub esp, 16
-
-		cmp dword [ebp+12], WM_CLOSE
-		je destroyWindow
-
-		cmp dword [ebp+12], WM_DESTROY
-		je postQuitMessage
-
 		mov dword eax, [ebp+8] 
 		mov dword [esp], eax
 		mov dword eax, [ebp+12]
@@ -139,21 +143,11 @@ section '.code' code readable executable
 		call [DefWindowProc]
 		jmp done
 
-		destroyWindow:
-			sub esp, 4
-			mov eax, [ebp+8]
-			mov dword [esp], eax
-			call [DestroyWindow]
-			mov dword eax, 0
-			jmp done
-
-		postQuitMessage:
+		destroy:
 			sub esp, 4
 			mov dword [esp], 0
 			call [PostQuitMessage]
-			mov dword eax, 0
-			jmp done
-
+			mov eax, 0
 		done:
 			pop ebp
 			ret
@@ -164,6 +158,7 @@ section '.code' code readable executable
 		mov ebp, esp
 
 		; Call the message box
+		sub esp, 16
 		mov dword [esp], NULL ; hWnd
 		mov dword eax, [ebp+8]
 		mov dword [esp+4], eax ; lpText
@@ -175,6 +170,21 @@ section '.code' code readable executable
 		; Restore previous stack frame
 		pop ebp
 		ret
+
+	start:
+		; Initialise the stack frame
+
+		push ebp
+		mov ebp, esp
+
+		call InitialiseWindow
+		call NewWindow
+		call NewWindow
+
+		sub esp, 4
+		mov dword [esp], 0
+		call [ExitProcess]
+
     
 section '.data' data readable writeable
     szClass 					db "CrossingMainWindow",0
