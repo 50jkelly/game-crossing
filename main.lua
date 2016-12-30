035b7400dc2bd0492a75218eaa0709e72cee269d
Jonathan Kelly <50jkelly@gmail.com>
2016-12-30 12:06:52 +0000

    Items in the world can be made of multiple parts that are drawn on separate frames

--- main.lua
+++ main.lua
@@ -11,27 +11,69 @@ controls["right"] = "d"
 
 -- Test trees
 tree1 = {}
+tree1["top"] = {}
+tree1["bottom"] = {}
 tree1["x"] = 100
 tree1["y"] = 100
-function tree1.draw()
-	love.graphics.draw(tree1["sprite"], tree1["x"], tree1["y"], 0, 0.2, 0.2, 0, 0)
+tree1["width"] = 182
+tree1["height"] = 200
+tree1["top"]["x"] = tree1["x"]
+tree1["top"]["y"] = tree1["y"]
+tree1["top"]["width"] = tree1["width"]
+tree1["top"]["height"] = 160
+tree1["top"]["layer"] = 2
+tree1["bottom"]["x"] = tree1["x"]
+tree1["bottom"]["y"] = tree1["y"] + tree1["top"]["height"]
+tree1["bottom"]["width"] = tree1["width"]
+tree1["bottom"]["height"] = 40
+tree1["bottom"]["layer"] = 1
+
+function tree1.top.draw()
+	love.graphics.draw(tree1["top"]["sprite"], tree1["top"]["x"], tree1["top"]["y"], 0, 1, 1, 0, 0)
+end
+
+function tree1.bottom.draw()
+	love.graphics.draw(tree1["bottom"]["sprite"], tree1["bottom"]["x"], tree1["bottom"]["y"], 0, 1, 1, 0, 0)
 end
 
 tree2 = {}
+tree2["top"] = {}
+tree2["bottom"] = {}
 tree2["x"] = 300
 tree2["y"] = 100
-function tree2.draw()
-	love.graphics.draw(tree2["sprite"], tree2["x"], tree2["y"], 0, 0.2, 0.2, 0, 0)
+tree2["width"] = 182
+tree2["height"] = 200
+tree2["top"]["x"] = tree2["x"]
+tree2["top"]["y"] = tree2["y"]
+tree2["top"]["width"] = tree2["width"]
+tree2["top"]["height"] = 160
+tree2["top"]["layer"] = 2
+tree2["bottom"]["x"] = tree2["x"]
+tree2["bottom"]["y"] = tree2["y"] + tree2["top"]["height"]
+tree2["bottom"]["width"] = tree2["width"]
+tree2["bottom"]["height"] = 40
+tree2["bottom"]["layer"] = 1
+
+function tree2.top.draw()
+	love.graphics.draw(tree2["top"]["sprite"], tree2["top"]["x"], tree2["top"]["y"], 0, 1, 1, 0, 0)
+end
+
+function tree2.bottom.draw()
+	love.graphics.draw(tree2["bottom"]["sprite"], tree2["bottom"]["x"], tree2["bottom"]["y"], 0, 1, 1, 0, 0)
 end
 
 -- Add items to the drawing layers
-layers.addItem(1, tree1)
-layers.addItem(2, tree2)
+layers.addItem(tree1["top"]["layer"], tree1["top"])
+layers.addItem(tree1["bottom"]["layer"], tree1["bottom"])
+layers.addItem(tree2["top"]["layer"], tree2["top"])
+layers.addItem(tree2["bottom"]["layer"], tree2["bottom"])
 
 function love.load()
 	player.load()
-	tree1["sprite"] = love.graphics.newImage("images/test_tree_1.png")
-	tree2["sprite"] = love.graphics.newImage("images/test_tree_1.png")
+	tree1["top"]["sprite"] = love.graphics.newImage("images/tree_1_top.png")
+	tree1["bottom"]["sprite"] = love.graphics.newImage("images/tree_1_bottom.png")
+	tree2["top"]["sprite"] = love.graphics.newImage("images/tree_1_top.png")
+	tree2["bottom"]["sprite"] = love.graphics.newImage("images/tree_1_bottom.png")
 end
 
 function love.update(dt)
