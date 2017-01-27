dee2143156f0e42a1d43e9f5d7cea3f0530cc7f0
Jonathan Kelly <50jkelly@gmail.com>
2017-01-27 19:45:58 +0000

    Did a lot of tidying up on the player code

--- scripts/player.lua
+++ scripts/player.lua
@@ -1,102 +1,42 @@
-local player = {}
-local playerId
+local this = {}
 
--- Hooks
-
-function player.keypressed(key)
-	local inventory = data.plugins.inventory
-	local items = data.plugins.items
-	local things = data.plugins.things
-	local dialogs = data.plugins.dialogs
-	local player = data.plugins.player
-	local sprites = data.plugins.sprites
-	local clock = data.plugins.clock
-
-	-- Player interaction
-
-	if data.state == 'game' and key == 'use' then
-
-		local heldItemSlot = inventory.getSlots()[inventory.highlightedSlot]
-		local heldItem = items.new[heldItemSlot.item]()
-		local playerRect = player.getRect()
-		local canInteract = things.getGroup('canInteract')
-
-		-- The jug
-
-		if heldItemSlot.item == 'jug' then
-			for _, thing in ipairs(canInteract) do
-				if thing.water and heldItem.charges == 0 then
-					dialogs.open('noWater')
-				elseif thing.water then
-					thing.water = 24 * 60
-					heldItem.charges = heldItem.charges - 1
-				end
-			end
-
-		-- The seed
-
-		elseif heldItemSlot.item == 'seed' then
-			inventory.removeItem(heldItemSlot)
-			local placed = copyThing(items.placeable)
-
-			if placed then
-				placed.placedTime = clock.getMinutes()
-				table.insert(things.thingsTable, placed)
-			end
-
-		-- No held item
-
-		else
-			for _, thing in ipairs(canInteract) do
-				if thing.pickupItems then
-					things.addToGroup(thing, 'toRemove')
-					for _, item in ipairs(thing.pickupItems) do
-						inventory.addItem(item)
-					end
-				end
-			end
-		end
-	end
-
+this.initialise = function()
+	this.vector = data.libraries.vector
+	this.x = 0
+	this.y = 0
 end
 
 -- Public Functions
 
-function player.getId()
-	return data.plugins.things.getGroup('player')[1].id
+function this.in_range(target_vector, range)
+	range = range or 0
+	local distance = (this.get_center - target_vector):len()
+	return distance <= range
 end
 
-function player.inRange(point2, range)
-	local things = data.plugins.things
+function this.rectangle()
+	local can_make_rectangle = 
+		this.x and
+		this.y and
+		this.width and
+		this.height
 
-	-- Get player data
-
-	if things then
-		local playerRect = player.getRect()
-		local range = range or things.getProperty(player.getId(), 'placementRange')
-
-		if playerRect and range then
-			local point1 = {
-				x = playerRect.x + playerRect.width / 2,
-				y = playerRect.y + playerRect.height / 2
-			}
-			return getDistance(point1, point2) <= range
-		end
+	if can_make_rectangle then
+		return {
+			x = this.x,
+			y = this.y,
+			width = this.width,
+			height = this.height,
+		}
 	end
+
 	return false
 end
 
-function player.getRect()
-	local things = data.plugins.things
-	local player = things.getGroup('player')[1]
-	if things and player then
-		return {
-			x = player.x or 0,
-			y = player.y or 0,
-			width = player.width or 0,
-			height = player.height or 0
-		}
-	end
+function this.get_center()
+	local center_x = this.x + this.width / 2
+	local center_y = this.y + this.height / 2
+	return vector(center_x, center_y)
 end
 
-return player
+return this
