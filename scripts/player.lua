2efee391118f2583f9f09d9dc7efbee978165e99
Jonathan Kelly <50jkelly@gmail.com>
2017-01-27 20:06:51 +0000

    Removed geometry functions from the player code

--- scripts/player.lua
+++ scripts/player.lua
@@ -14,29 +14,4 @@ function this.in_range(target_vector, range)
 	return distance <= range
 end
 
-function this.rectangle()
-	local can_make_rectangle = 
-		this.x and
-		this.y and
-		this.width and
-		this.height
-
-	if can_make_rectangle then
-		return {
-			x = this.x,
-			y = this.y,
-			width = this.width,
-			height = this.height,
-		}
-	end
-
-	return false
-end
-
-function this.get_center()
-	local center_x = this.x + this.width / 2
-	local center_y = this.y + this.height / 2
-	return vector(center_x, center_y)
-end
-
 return this
